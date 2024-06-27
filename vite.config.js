404a693fc27d941aaeaa5e22da083ef8a9aa29d9
Gede Dhanu Purnayasa <dhanuprys@gmail.com>
2024-06-27 17:02:05 +0800

    Major update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import laravel, { refreshPaths } from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/mantram-accordion.js',
+                'resources/js/mantram-audio.js',
+                'resources/js/sidebar.js'
+            ],
             refresh: [
                 ...refreshPaths,
                 'app/Filament/**',
