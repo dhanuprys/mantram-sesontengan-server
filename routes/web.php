f1ef58b682818f2980e843e661ff78258cec8428
Gede Dhanu Purnayasa <dhanuprys@gmail.com>
2024-07-08 09:54:52 +0800

    Add official API endpoint for mobile apps

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,62 @@ Route::view('/', 'main');
 Route::view('/mantram{all}', 'main')->where('all', '.*');
 
 Route::prefix('/api')->group(function () {
+    Route::prefix('v1')->group(function () {
+        Route::get('/mantram', function () {
+            $output = [];
+            $mantramBases = MantramBase::select(['id', 'name'])->get();
+
+            foreach ($mantramBases as $mantram) {
+                $output[] = [
+                    'id' => $mantram->id,
+                    'name' => $mantram->name,
+                    'mantram_count' => Mantram::where('mantram_base_id', $mantram->id)->count()
+                ];
+            }
+
+            return response()->json($output);
+        });
+
+        Route::get('/mantram/{mantramBaseId}', function ($mantramBaseId) {
+            $mantrams = Mantram::where('mantram_base_id', $mantramBaseId)
+                ->select([
+                    'id',
+                    'name',
+                    'mantram',
+                    'description'
+                ])
+                ->get();
+
+            if ($mantrams->count() <= 0) {
+                return response()->json(null, 404);
+            }
+
+            return response()->json($mantrams);
+        })->name('mantram-base');
+
+        Route::get('/mantram/{mantramBaseId}/{mantramId}', function ($mantramBaseId, $mantramId) {
+            $mantram = Mantram::where([
+                'id' => $mantramId,
+                'mantram_base_id' => $mantramBaseId,
+            ])
+            ->select([
+                'id',
+                'name',
+                'mantram',
+                'description',
+                'audio_url',
+                'updated_at'
+            ])
+            ->first();
+
+            if ($mantram->count() <= 0) {
+                return response()->json(null, 404);
+            }
+
+            return response()->json($mantram);
+        })->name('mantram-detail');
+    });
+
     Route::get('/mantram', function () {
         $output = [];
         $mantramBases = MantramBase::select(['id', 'name'])->get();
